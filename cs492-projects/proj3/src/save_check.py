ae55efe0fc0a0d92364ca5751e5567b416545868
tink-expo <whhan8485@gmail.com>
2020-07-05 20:21:28 +0900

    File cleanup

--- cs492-projects/proj3/src/save_check.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-for i in range(40):
-    p1 = './intermediate-1/layer_{}.npy'.format(i)
-    p2 = './intermediate/layer_{}.npy'.format(i)
-    print(abs(np.load(p1) - np.load(p2)).max())
